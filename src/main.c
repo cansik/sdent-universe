614c53ef8d19659ce3c9ecc65a0a561f1b6d763d
Florian Bruggisser <florian@nexpose.ch>
2016-04-06 23:39:17 +0200

    separated player update

--- src/main.c
+++ src/main.c
@@ -18,18 +18,47 @@ typedef struct player
 void renderPlayer(Player *p)
 {
   box(p->x, p->y, p->x + p->w, p->y + p->h, M_FILL);
-  //plot_point(p->x, p->y);
+}
+
+void updatePlayer(Player *p)
+{
+  UBYTE x,y,i;
+
+  i = joypad();
+
+  x = p->x;
+  y = p->y;
+
+  if(i & J_UP)
+     y -= 1;
+  if(i & J_DOWN)
+     y += 1;
+  if(i & J_LEFT)
+     x -= 1;
+  if(i & J_RIGHT)
+     x += 1;
+
+  // check bounds
+  if(x > 0 && x + p->w < GRAPHICS_WIDTH)
+  {
+      p->x = x;
+  }
+
+  if(y > 0 && y + p->h < GRAPHICS_HEIGHT)
+  {
+      p->y = y;
+  }
 }
 
 void main() {
-    UBYTE c,x,y,i;
+    UBYTE c;
     Player p = {50, 50, 10, 10};
 
     c=50;
 
     printf("The Vacuumizor");
     printf("\nPress Start");
-    waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
+    waitpad(J_START);
 
     color(LTGREY,WHITE,SOLID);
 
@@ -37,43 +66,8 @@ void main() {
     {
       //draw
       renderPlayer(&p);
-      color(LTGREY,WHITE,SOLID);
-      i = joypad();
-      x = p.x;
-      y = p.y;
-      if(i & J_UP)
-	       y -= 1;
-      if(i & J_DOWN)
-	       y += 1;
-      if(i & J_LEFT)
-	       x -= 1;
-      if(i & J_RIGHT)
-	       x += 1;
-
-      // check bounds
-      if(x > 0 && x + p.w < GRAPHICS_WIDTH)
-      {
-          p.x = x;
-      }
-
-      if(y > 0 && y + p.h < GRAPHICS_HEIGHT)
-      {
-          p.y = y;
-      }
+      updatePlayer(&p);
 
       wait_vbl_done();
-
-      /*
-      for (c=0; c<=10; c++) {
-        box(140,c*10+15+x,15, 15 + x,M_NOFILL);
-        //gprintf("%c",c++);
-      }
-
-      //color(WHITE,LTGREY,SOLID);
-      //box(0, 0, WINSZX, WINSZY, M_FILL);
-
-      x++;
-      //x *= -1;
-      */
     }
 }
