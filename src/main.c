b1193ecbd1fea55abfd99504b69d780720391ea6
unknown <grisu118@grisu118.ch>
2016-04-06 12:47:46 +0200

    Revert "Remove not working min/max funcitons"
    This reverts commit 29d6eebc8cdb3d489c576f65079add1d8c341e65.

--- src/main.c
+++ src/main.c
@@ -21,6 +21,14 @@ void renderPlayer(Player *p)
   //plot_point(p->x, p->y);
 }
 
+UBYTE min(UBYTE a, UBYTE b) {
+    return a<b ? a : b;
+}
+
+UBYTE max(UBYTE a, UBYTE b) {
+    return a>b ? a : b;
+}
+
 void main() {
     UBYTE c,x,y,i;
     Player p = {50, 50, 10, 10};
@@ -37,7 +45,7 @@ void main() {
     {
       //draw
       renderPlayer(&p);
-
+      color(LTGREY,WHITE,SOLID);
       i = joypad();
       x = p.x;
       y = p.y;
