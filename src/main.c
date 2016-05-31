60c5fbd118e764e2f57e37c55dc061f7292e6f53
Grisu118 <grisu118@grisu118.ch>
2016-05-31 14:10:24 +0200

    Story at start

--- src/main.c
+++ src/main.c
@@ -7,23 +7,26 @@
 #include "menu/menu.h"
 #include "game/game.h"
 #include "about/about.h"
+#include "story/story.h"
 
 void main() {
-  UBYTE choice;
+    UBYTE choice;
+    
+    displayStory();
     
     while(1)
     {
-      choice = mainMenu();
-      switch(choice) {
-        case 1:
-          play();
-          break;
-        case 2:
-          //Highscore
-          break;
-        case 3:
-          displayAbout();
-          break;
+        choice = mainMenu();
+        switch(choice) {
+          case 1:
+              play();
+              break;
+          case 2:
+              //Highscore
+              break;
+          case 3:
+              displayAbout();
+              break;
       }
       
 
--- src/story/storyTiles.h
+++ src/story/storyTiles.h
@@ -1,1 +1,7 @@
-extern unsigned char storyTiles[];
+#define STORY1COUNT 199
+#define STORY2COUNT 210
+#define STORY3COUNT 158
+
+extern unsigned char story1Tiles[];
+extern unsigned char story2Tiles[];
+extern unsigned char story3Tiles[];
