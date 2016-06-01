2be70d559f1fcc7f15f4f6ac48f2f56495098c68
joel <morbrolhc@gmail.com>
2016-06-01 09:56:26 +0200

    Added delay in Story Screens. Concerning Issue #6.

--- src/story/story.c
+++ src/story/story.c
@@ -5,66 +5,72 @@
 #include "storyTiles.h"
 #include "storyMap.h"
 
+#define STORY_DELAY 150
+
 void loadStory1() {
     DISPLAY_OFF;
 	HIDE_BKG;
     HIDE_SPRITES;
-    
+
     set_bkg_data( 0, STORY1COUNT, story1Tiles );
     VBK_REG = 1;
 
     VBK_REG = 0;
     set_bkg_tiles(0,0,storyMapWidth,storyMapHeight,story1Map);
-    
+
     SHOW_BKG;
     DISPLAY_ON;
+    delay(STORY_DELAY);
 }
 
 void loadStory2() {
     DISPLAY_OFF;
 	HIDE_BKG;
     HIDE_SPRITES;
-    
+
     set_bkg_data( 0, STORY2COUNT, story2Tiles );
-    
+
     VBK_REG = 1;
 
     VBK_REG = 0;
     set_bkg_tiles(0,0,storyMapWidth,storyMapHeight,story2Map);
-    
+
     SHOW_BKG;
     DISPLAY_ON;
+    delay(STORY_DELAY);
+
 }
 
 void loadStory3() {
     DISPLAY_OFF;
 	HIDE_BKG;
     HIDE_SPRITES;
-    
+
     set_bkg_data( 0, STORY3COUNT, story3Tiles );
-    
+
     VBK_REG = 1;
 
     VBK_REG = 0;
     set_bkg_tiles(0,0,storyMapWidth,storyMapHeight,story3Map);
-    
+
     SHOW_BKG;
     DISPLAY_ON;
+    delay(STORY_DELAY);
 }
 
 void loadStory(UBYTE i) {
     if (i == 1) {
         loadStory1();
         return;
-    } 
+    }
     if (i == 2) {
         loadStory2();
         return;
-    } 
+    }
     if (i == 3) {
         loadStory3();
         return;
-    } 
+    }
 }
 
 void displayStory() {
