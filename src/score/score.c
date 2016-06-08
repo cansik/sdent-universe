da592fed106fb6886bc5c9dce6c6f38ccabf54aa
Grisu118 <grisu118@grisu118.ch>
2016-06-08 13:31:25 +0200

    fixed

--- src/score/score.c
+++ src/score/score.c
@@ -3,35 +3,35 @@
 void displayScore() {}
 
 UBYTE getTileNumberFromNumber(UBYTE num) {
-    return num + 14;
+    return num + 24;
 }
 
 void initScore() {
     SPRITES_8x8;      /* sets sprites to, yep you guessed it, 8x16 mode */
 
-    set_sprite_data(10, 14, scoreTiles);   /* defines the sprite data */
-    set_sprite_tile(10,10);
-    set_sprite_tile(11,11);
-    set_sprite_tile(12,12);
-    set_sprite_tile(13,13);
-    set_sprite_tile(14,getTileNumberFromNumber(0));
-    set_sprite_tile(15,getTileNumberFromNumber(0));
-    set_sprite_tile(16,getTileNumberFromNumber(0));
+    set_sprite_data(20, 14, scoreTiles);   /* defines the sprite data */
+    set_sprite_tile(20,20);
+    set_sprite_tile(21,21);
+    set_sprite_tile(22,22);
+    set_sprite_tile(23,23);
+    set_sprite_tile(24,getTileNumberFromNumber(0));
+    set_sprite_tile(25,getTileNumberFromNumber(0));
+    set_sprite_tile(26,getTileNumberFromNumber(0));
 }
 
 void renderScore(Score *s) {
     if (s->score < 10) {
-        set_sprite_tile(16, getTileNumberFromNumber(s->score));
+        set_sprite_tile(26, getTileNumberFromNumber(s->score));
     }
 
 
-    move_sprite(10,108,148); 
-    move_sprite(11,116,148); 
-    move_sprite(12,124,148); 
-    move_sprite(13,132,148); 
-    move_sprite(14,140,148); 
-    move_sprite(15,148,148); 
-    move_sprite(16,156,148); 
+    move_sprite(20,108,148); 
+    move_sprite(21,116,148); 
+    move_sprite(22,124,148); 
+    move_sprite(23,132,148); 
+    move_sprite(24,140,148); 
+    move_sprite(25,148,148); 
+    move_sprite(26,156,148); 
 
 
 }
